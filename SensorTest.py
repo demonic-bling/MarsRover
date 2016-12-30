594303b32c2610ad7e058a4e255725459fda98f6
demonic-bling <gauthamvinod@outlook.com>
2016-12-30 13:06:45 +0530

    PWM added to all motors

--- SensorTest.py
+++ SensorTest.py
@@ -15,28 +15,36 @@ USSEcho1 = 25
 
 print "Distance Measurement In Progress"
 
-GPIO.setup(Motor1A, GPIO.OUT)
-GPIO.setup(Motor2A, GPIO.OUT)
-GPIO.setup(Motor1B, GPIO.OUT)
-GPIO.setup(Motor2B, GPIO.OUT)
+pwm1 = GPIO.PWM(Motor1A, 1000)
+pwm2 = GPIO.PWM(Motor2A, 1000)
+pwm3 = GPIO.PWM(Motor1B, 1000)
+pwm4 = GPIO.PWM(Motor2B, 1000)
+
+pwm1.start(0)
+pwm2.start(0)
+pwm3.start(0)
+pwm4.start(0)
 	
 GPIO.setup(USSTrig1, GPIO.OUT)
 GPIO.setup(USSEcho1, GPIO.IN)
 
 GPIO.output(USSTrig1, False)
+print 'Waiting for sensors to settle'
 time.sleep(2)
 
 def forward():
-	GPIO.output(Motor1A, GPIO.HIGH)
-	GPIO.output(Motor1B, GPIO.LOW)
-	GPIO.output(Motor2A, GPIO.HIGH)
-	GPIO.output(Motor2B, GPIO.LOW)
+	pwm1.ChangeDutyCycle(50)
+	pwm2.ChangeDutyCycle(50)
+	pwm3.ChangeDutyCycle(0)
+	pwm4.ChangeDutyCycle(0)
 
 def reverse():
-	GPIO.output(Motor1A, GPIO.LOW)
-	GPIO.output(Motor1B, GPIO.HIGH)
-	GPIO.output(Motor2A, GPIO.LOW)
-	GPIO.output(Motor2B, GPIO.HIGH)
+	pwm1.ChangeDutyCycle(0)
+	pwm2.ChangeDutyCycle(0)
+	pwm3.ChangeDutyCycle(50)
+	pwm4.ChangeDutyCycle(50)
+
+
 
 def stop():
 	GPIO.output(Motor1A, GPIO.LOW)
@@ -45,18 +53,19 @@ def stop():
 	GPIO.output(Motor2B, GPIO.LOW)
 
 def Left():
-	GPIO.output(Motor1A, GPIO.LOW)
-	GPIO.output(Motor2A, GPIO.HIGH)
-	GPIO.output(Motor1B, GPIO.HIGH)
-	GPIO.output(Motor2B, GPIO.LOW)
+	pwm1.ChangeDutyCycle(0)
+	pwm2.ChangeDutyCycle(90)
+	pwm3.ChangeDutyCycle(90)
+	pwm4.ChangeDutyCycle(0)
 		
 def Right():
-    GPIO.output(Motor1A, GPIO.HIGH)
-    GPIO.output(Motor2A, GPIO.LOW)
-    GPIO.output(Motor1B, GPIO.LOW)
-    GPIO.output(Motor2B, GPIO.HIGH)
-
+    pwm1.ChangeDutyCycle(90)
+	pwm2.ChangeDutyCycle(0)
+	pwm3.ChangeDutyCycle(0)
+	pwm4.ChangeDutyCycle(90)
+		
 def USS1Distance():	
+	time.sleep(1)
 	GPIO.output(USSTrig1, GPIO.HIGH)
 	time.sleep(0.00001)
 	GPIO.output(USSTrig1, GPIO.LOW)
@@ -75,7 +84,7 @@ while True:
 	forward()
 	status = USS1Distance()
 	print status
-	if(status < 30):
+	if(status < 40):
 		reverse()
 		time.sleep(0.75)
 		Left()
