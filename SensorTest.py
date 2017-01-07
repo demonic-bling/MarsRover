d315ba1a76b48e202befa3a461fc06d475de27d4
demonic-bling <gauthamvinod@outlook.com>
2017-01-07 11:56:44 +0530

    Added code for back sensor

--- SensorTest.py
+++ SensorTest.py
@@ -13,6 +13,9 @@ Motor2B = 23	#Right Side Motor In2
 USSTrig1 = 9
 USSEcho1 = 25
 
+USSTrig2 = 11
+USSEcho2 = 8
+
 print "Distance Measurement In Progress"
 
 GPIO.cleanup()
@@ -35,8 +38,11 @@ pwm4.start(0)
 	
 GPIO.setup(USSTrig1, GPIO.OUT)
 GPIO.setup(USSEcho1, GPIO.IN)
+GPIO.setup(USSTrig2, GPIO.OUT)
+GPIO.setup(USSEcho2, GPIO.IN)
 
 GPIO.output(USSTrig1, False)
+GPIO.output(USSTrig2, False)
 print 'Waiting for sensors to settle'
 time.sleep(2)
 
@@ -86,18 +92,41 @@ def USS1Distance():
 	distance = pulse_duration * 17150
 	return distance
 
+def USS2Distance():	
+	time.sleep(1)
+	GPIO.output(USSTrig2, GPIO.HIGH)
+	time.sleep(0.00001)
+	GPIO.output(USSTrig2, GPIO.LOW)
+
+	while GPIO.input(USSEcho2) == 0:
+		pulse_start = time.time()
+
+	while GPIO.input(USSEcho2) == 1:
+		pulse_end = time.time()
+
+	pulse_duration = pulse_end - pulse_start
+	distance = pulse_duration * 17150
+	return distance
+
 try:
 	while True:
 		forward()
-		status = USS1Distance()
-		print status
-		if(status < 60):
-			reverse()
-			time.sleep(0.75)
-			Left()
-			time.sleep(0.75)
-			stop()
-			
+		fwdDist = USS1Distance()
+		bwdDist = USS2Distance()
+		print "Front", fwdDist
+		print "Back", bwdDist
+		if(fwdDist < 70):
+			if(bwdDist > 50):
+				reverse()
+				time.sleep(0.75)
+				Left()
+				time.sleep(0.75)
+				stop()
+			else:
+				Left()
+				time.sleep(1)
+				stop()
+				
 except KeyboardInterrupt:
 	pass
 
